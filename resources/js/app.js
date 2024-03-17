a7b4cf7e3d58fc3df15ac9afb0ce709b0ab072b8
Thiago Tenório <thitccp@gmail.com>
2024-03-17 14:14:07 -0300

    add font dependencies and inertia

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,11 @@ import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faBell } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+library.add(faBell);
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -12,10 +17,15 @@ createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
     setup({ el, App, props, plugin }) {
-        return createApp({ render: () => h(App, props) })
+        const app = createApp({ render: () => h(App, props) })
             .use(plugin)
-            .use(ZiggyVue)
-            .mount(el);
+            .use(ZiggyVue);
+
+        app.component('FontAwesomeIcon', FontAwesomeIcon);
+
+        app.mount(el);
+
+        return app;
     },
     progress: {
         color: '#4B5563',
