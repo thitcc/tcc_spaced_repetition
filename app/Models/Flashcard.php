64296d315e710ea22cb806f628dd0f1a897b68bd
Thiago Tenório <thitccp@gmail.com>
2024-03-17 14:35:20 -0300

    qol changes to flashcard model

--- app/Models/Flashcard.php
+++ app/Models/Flashcard.php
@@ -10,11 +10,27 @@ class Flashcard extends Model
     use HasFactory;
 
     protected $fillable = [
-        'subject_id', 'question', 'option_a', 'option_b', 'option_c', 'option_d', 'correct_answer'
+        'subject_id', 
+        'question', 
+        'option_a', 
+        'option_b', 
+        'option_c', 
+        'option_d', 
+        'correct_answer'
     ];
 
     public function subject()
     {
         return $this->belongsTo(Subject::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function isCorrect($option)
+    {
+        return $this->correct_answer === $option;
     }
 }
