824ce7eea55c19207f4c2bc514fb512dc070545b
Thiago Costa <thiagocosta@fetch.ly>
2024-10-25 03:49:40 -0300

    remove time limit

--- resources/js/Pages/CourseShow.vue
+++ resources/js/Pages/CourseShow.vue
@@ -98,20 +98,6 @@ function addStudents() {
               required
             />
           </div>
-          <div class="mb-4">
-            <label
-              class="block text-gray-700 text-sm font-bold mb-2"
-              for="time_limit"
-            >
-              Tempo Limite (em segundos)
-            </label>
-            <input
-              v-model="subjectForm.time_limit"
-              type="number"
-              id="time_limit"
-              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            />
-          </div>
           <div class="flex items-center justify-between">
             <button
               class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
